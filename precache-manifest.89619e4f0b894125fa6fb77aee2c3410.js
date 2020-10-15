1e80fb63a4defd46a293c3cd360120b361e65a30
Adrian Ciura <aciura@gmail.com>
2020-10-15 09:24:04 +0200

    Updates

---
+++ precache-manifest.89619e4f0b894125fa6fb77aee2c3410.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0f3f4fbec8a79ff4b8982667b1b0393",
+    "url": "/governace-app/index.html"
+  },
+  {
+    "revision": "d667b22205cea5681bb9",
+    "url": "/governace-app/static/css/main.d28deb60.chunk.css"
+  },
+  {
+    "revision": "78b4e7dbbd056e94623f",
+    "url": "/governace-app/static/js/2.637b86c0.chunk.js"
+  },
+  {
+    "revision": "5ba18ce0143bcf4b3e82bfc225c31966",
+    "url": "/governace-app/static/js/2.637b86c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d667b22205cea5681bb9",
+    "url": "/governace-app/static/js/main.da7920be.chunk.js"
+  },
+  {
+    "revision": "0b3751501b406d169d3e",
+    "url": "/governace-app/static/js/runtime-main.92ae714c.js"
+  },
+  {
+    "revision": "bc22a0cbc62fd7330800485725771555",
+    "url": "/governace-app/static/media/Capitol-1920px-sky.bc22a0cb.jpg"
+  }
+]);
